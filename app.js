125c0c45e0854df0ffca61df92ff993dee72148d
christophegourmand <christophe.gourmand@gmail.com>
2021-10-30 17:45:21 +0200

    fix project organisation (files tree)

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const monModuleImporte = require('./monmodule');
+const monModuleImporte = require('./monModule');
 const http = require('http');
 
 console.log(process.env.NODE_ENV);
@@ -18,7 +18,7 @@ console.log(process.env.NODE_ENV);
 
 // CHAPTER concerning Events : test with events
     // imports our file
-    const file_eventEmitter_exemple = require('./eventEmitter_exemple');
+    const file_eventEmitter_exemple = require('./events/eventEmitter_exemple');
     
     // on the 'timer' who emit events >> we attach a listener for event '3_secondes', who will run a callback function with a console.log.
     file_eventEmitter_exemple.timer.on( '3_secondes', ()=>console.log('3secondes écoulées') );
